527920d35323a3092cc81514ece46e1fedffb735
alirni <aliramezani19.1@gmail.com>
2016-11-17 14:46:10 +0330

    feat(box-menu-icon): VA-10 event fire

--- src/box-menu-icon.js
+++ src/box-menu-icon.js
@@ -3,7 +3,28 @@
 
   class BoxMenuIcon extends Polymer.Element {
     static get is() { return 'box-menu-icon'; }
+
+    static get config () {
+      return {
+        properties: {
+          // myEvent: String,
+        },
+        observers: [
+        ]
+      };
+    }
+
+    ready () {
+      super.ready();
+      var s = this.$.one;
+      s.addEventListener('fireevent', this.myEvent);
+    }
+
+    // myEvent () {
+    //   console.log('myEvent');
+    // }
   }
 
   customElements.define(BoxMenuIcon.is, BoxMenuIcon);
+
 })();
