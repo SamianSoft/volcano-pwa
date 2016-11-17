abadd194613495db662a07d652f491b36354b726
alirni <aliramezani19.1@gmail.com>
2016-11-17 12:59:55 +0330

    feat(box-menu-icon.js): VA-10 create box-menu-icon.js

---
+++ src/box-menu-icon.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+
+  class BoxMenuIcon extends Polymer.Element {
+    static get is() { return 'box-menu-icon'; }
+  }
+
+  customElements.define(BoxMenuIcon.is, BoxMenuIcon);
+})();
